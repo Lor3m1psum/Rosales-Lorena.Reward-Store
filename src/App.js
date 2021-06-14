8800e874ddcfc9be2944b09c8e0806a128086e46
Lor3m1psum <lorena.rosales.pajaro87@gmail.com>
2021-06-14 17:33:00 -0500

    dependencies & Assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+<h1>Hello world</h1>
     </div>
   );
 }
---
+++ src/Headers.js
@@ -0,0 +1,4 @@
+const headers = {
+  "content-type":"application/json",
+  Authorization: "Beare reyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk"
+}
