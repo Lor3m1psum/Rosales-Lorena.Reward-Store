8069441428fca2f64277fc615e0423be7e67c45c
Lor3m1psum <lorena.rosales.pajaro87@gmail.com>
2021-08-27 17:22:27 -0500

    commit on master

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <App />,
+  <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
